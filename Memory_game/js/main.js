15aa04cdd91f738b1265ad8fe36fbf346a45e739
Wilfred Loh <lohwilfred19@gmail.com>
2019-06-12 02:48:51 +0800

    v3 updated with functions

--- Memory_game/js/main.js
+++ Memory_game/js/main.js
@@ -3,36 +3,37 @@ console.log('Up and running!');
 let cards = ['queen','queen', 'king', 'king'];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped '+ cardOne);
-console.log('User flipped '+ cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[0]) {
-		alert('You found a match!');
-	} else if (cardsInPlay[0] === cardsInPlay[1]){
-		alert('Sorry,try again!');
+function checkForMatch () {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry,try again!");
+		}
 	}
 }
 
-console.log(cardsInPlay)
+function flipCard (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
 
+flipCard(0);
+flipCard(2);
 
+console.log(5);
 
+/*
+	let cardOne = cards[0];
+	cardsInPlay.push(cardOne);
 
+	let cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
 
-
-
-
-
-
-
-
+	console.log('User flipped '+ cardOne);
+	console.log('User flipped '+ cardTwo);
+*/
 /* 
 
 // Trial ONE --------------------------------------
